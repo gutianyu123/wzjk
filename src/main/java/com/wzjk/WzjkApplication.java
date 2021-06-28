0aee32f52fe44c4cc3d5f1f584c16dc5b9c6c2e5
xiaogu <1220724521@qq.com>
2021-06-29 00:02:19 +0800

    增加https请求配置

--- src/main/java/com/wzjk/WzjkApplication.java
+++ src/main/java/com/wzjk/WzjkApplication.java
@@ -1,8 +1,14 @@
 package com.wzjk;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,5 +19,32 @@ public class WzjkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WzjkApplication.class, args);
 	}
+
+	@Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
+        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint=new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection=new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }
+
+	@Bean
+    public Connector connector(){
+        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8101);
+        connector.setSecure(false);
+        connector.setRedirectPort(8100);
+        return connector;
+    }
 
 }
